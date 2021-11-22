a7356b73ce24f825fdd096a7dd5b95b475e79109
morrkvot <limpan92_2@hotmail.com>
2021-11-23 00:48:11 +0900

    Removed DB credentials

--- next.config.js
+++ next.config.js
@@ -3,6 +3,5 @@ module.exports = {
   env: {
     // connection string
     MONGO_URI:
-      "mongodb+srv://morrkvot:Sarina-0617%40@cluster0.v63tx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   },
 };
